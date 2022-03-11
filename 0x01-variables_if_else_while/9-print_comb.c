11d8ac97194710e85747c435d873434b908c915d
Geekbooi <yosephmekonnen2@gmail.com>
2022-03-11 13:27:21 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,9 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar(i % 10 + '0');			
+		putchar(i % 10 + '0');
 		if (i < 9)
-		{	
-																putchar(',');
+		{														putchar(',');
 																putchar(' ');
 		}
 	}
