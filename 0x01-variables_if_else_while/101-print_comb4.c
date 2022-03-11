2ad59c23293e48a67f3b768a4ad4d5650d996192
Geekbooi <yosephmekonnen2@gmail.com>
2022-03-11 13:50:08 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,13 +13,15 @@ int main(void)
 		for (j = i + 1; j <= '9'; j++)
 		{
 			for (k = j + 1; k <= '9'; k++)
-			putchar(i);
-			putchar(j);
-			putchar(k);
-			if (i != '7' || j != '8' || k != '9')
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != '7' || j != '8' || k != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
