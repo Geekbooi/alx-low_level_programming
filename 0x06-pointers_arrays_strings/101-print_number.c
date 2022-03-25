078b80da28ff5b05108847d1b8cb8b14bba654dc
Geekbooi <yosephmekonnen2@gmail.com>
2022-03-25 14:53:42 -0700

    final pointers and string

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ /*
+  * write your line of code here...
+  * main-returns void
+  * Remember:
+  * You can only use _putchar function to print
+  * You are not allowed to use arrays or pointers
+  * You are not allowed to hard-code special values
+  * scanf accepts input from user
+  * print_number prints an integer
+  */
+void print_number(int n);
+ 
+int main()
+{
+    int n;
+    printf("Enter a number\n");
+    scanf("%d", &n);
+    
+    printNumber(n);
+    
+    return 0
+}
