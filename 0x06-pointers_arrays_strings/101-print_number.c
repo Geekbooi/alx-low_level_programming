6581989afc68c040c0d535b3a93dcd7a54f659f2
Geekbooi <yosephmekonnen2@gmail.com>
2022-03-25 15:18:03 -0700

    101 print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
- /*
-  * write your line of code here...
-  * main-returns void
-  * Remember:
-  * You can only use _putchar function to print
-  * You are not allowed to use arrays or pointers
-  * You are not allowed to hard-code special values
-  * scanf accepts input from user
-  * print_number prints an integer
-  */
-void print_number(int n);
- 
-int main()
+
+int main(void)
 {
-    int n;
-    printf("Enter a number\n");
-    scanf("%d", &n);
-    
-    printNumber(n);
-    
-    return 0
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+	;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
