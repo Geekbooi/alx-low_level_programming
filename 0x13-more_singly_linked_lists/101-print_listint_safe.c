508e1cbc2112fd5ea26591b440d24bff1f4e90a9
Geekbooi <yosephmekonnen@gmail.com>
2022-04-27 10:03:45 +0300

    0x13

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -22,6 +22,7 @@ size_t looped_listint_len(const listint_t *head)
 
 	tortoise = head->next;
 	hare = (head->next)->next;
+
 	while (hare)
 	{
 		if (tortoise == hare)
@@ -29,7 +30,6 @@ size_t looped_listint_len(const listint_t *head)
 			tortoise = head;
 			while (tortoise != hare)
 			{
-
 				nodes++;
 				tortoise = tortoise->next;
 				hare = hare->next;
@@ -53,36 +53,36 @@ size_t looped_listint_len(const listint_t *head)
 }
 
 /**
- *  * print_listint_safe - Prints a listint_t list safely.
- *   * @head: A pointer to the head of the listint_t list.
- *    *
- *     * Return: The number of nodes in the list.
- *      */
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of nodes in the list.
+ */
 size_t print_listint_safe(const listint_t *head)
 {
-		size_t nodes, index = 0;
+	size_t nodes, index = 0;
 
-			nodes = looped_listint_len(head);
+	nodes = looped_listint_len(head);
 
-				if (nodes == 0)
-						{
-									for (; head != NULL; nodes++)
-												{
-																printf("[%p] %d\n", (void *)head, head->n);
-																			head = head->next;
-																					}
-										}
+	if (nodes == 0)
+	{
+		for (; head != NULL; nodes++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+	}
 
-					else
-							{
-										for (index = 0; index < nodes; index++)
-													{
-																	printf("[%p] %d\n", (void *)head, head->n);
-																				head = head->next;
-																						}
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+	
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
 
-												printf("-> [%p] %d\n", (void *)head, head->n);
-													}
-
-						return (nodes);
+	return (nodes);
 }
