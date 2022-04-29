1dbd318dd2b711d9a0f210e89ec5049f3ad342ca
Geekbooi <yosephmekonnen@gmail.com>
2022-04-29 08:23:46 +0300

    0x14-bit_manipulation

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -11,11 +11,9 @@
  */
 int get_endianness(void)
 {
-	int num = 1;
-	char *endian = (char *)&num;
+	unsigned int n = 1;
 
-	if (*endian == 1)
+	if (*(char *) &n == 1)
 		return (1);
-
 	return (0);
 }
