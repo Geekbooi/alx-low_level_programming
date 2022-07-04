e2a7b7b35219c2c6fbc5f0c3e9f233c3cf4f30a0
Geekboii <99346437+Geekbooi@users.noreply.github.com>
2022-07-05 01:33:48 +0300

    fixing indentation for the base geometry
    fixing indentation for the base geometry

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,11 +1,10 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Creates a hash table.
- * @size: The size of the array.
- *
- * Return: If an error occurs - NULL.
- *         Otherwise - a pointer to the new hash table.
+ * hash_table_create - creates a hash table
+ * @size: the size of the array to create
+ * Return: pointer to the newly created hash table or NULL on
+ * failure
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
@@ -15,6 +14,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table = malloc(sizeof(hash_table_t));
 	if (!hash_table)
 		return (NULL);
+
 	hash_table->size = size;
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (!hash_table->array)
